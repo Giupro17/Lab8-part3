b1102bcbb5029bfd8dc49a923f1b5d698baf00e8
Giuliano <gproiettu07@icloud.com>
2025-03-28 10:48:08 -0400

    part 3

--- CalcEngineTester.java
+++ CalcEngineTester.java
@@ -63,3 +63,18 @@ public class CalcEngineTester
         return engine.getDisplayValue();
     }
 }
+/*
+ * 22. I beleive what the final line says because it matches the return value 
+ * that is in the code and all the calculations were executed.
+ * 23. returns 1 the first time, then 7 the second, it should alway return 7.
+ * 24. it gives the correct result the first time then it keeps subracting
+ * from the returned value from the previous test.
+ * 26,
+ * Method Called	displayValue	leftOperand	previousOperator
+ * ---------------------------------------------------------------------
+ * initial state             0               0               ''
+ * plus()                    0               0               '+'
+ * numberPressed(4)          4               0               '+'
+ * equals()                  4               4               ''
+ * gwtDisplayValue           4               4               ''
+ */
